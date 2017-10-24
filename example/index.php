401336f3c7699d06d6e7e5b118bfdde528b3a038
Reza Juliandri <rezajuliandri20@gmail.com>
2017-10-24 13:15:50 +0700

    Add More random type

--- example/index.php
+++ example/index.php
@@ -1,5 +1,18 @@
 <?php
 require_once('../src/Str.php');
+//
 // Example Random
-// Usage for Token or Random alphanumberic
-echo Str::random(32);
+//
+
+// Default Usage
+echo Str::random(18)."\n";
+// Alphanumeric
+echo Str::random(18,'alnum')."\n";
+// Alphabet
+echo Str::random(18,'alpha')."\n";
+// Numeric
+echo Str::random(18,'numeric')."\n";
+// MD5
+echo Str::random(null,'md5')."\n";
+// Not Defined Random String
+echo Str::random(null,'nothing')."\n";
--- src/Str.php
+++ src/Str.php
@@ -4,14 +4,54 @@
  */
 class Str
 {
-  public static function random($length = 16)
+  /*
+  * Type Of Random
+  * - alnum = Alphanumeric
+  * - alpha = Alphabet
+  * - numeric
+  * - md5 ($length always 32 character)
+  * - hex ($length must be even)
+  */
+  public static function random($length = 16,$type='alnum')
     {
-        $string = '';
-        while (($len = strlen($string)) < $length) {
-            $size = $length - $len;
-            $bytes = random_bytes($size);
-            $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
-        }
+      $string = '';
+      switch ($type) {
+        case 'alnum':
+            while (($len = strlen($string)) < $length) {
+              $size = $length - $len;
+              $bytes = random_bytes($size);
+              $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+            }
+          break;
+        case 'alpha':
+            $data = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            while(($len = strlen($string))< $length){
+              $string .= $data[rand(0,strlen($data)-1)];
+              //$string .= $data[rand(0,strlen(($data)-1))];
+            }
+          break;
+        case 'numeric':
+            $data = "01234567890";
+            while (($len = strlen($string))< $length) {
+              $string .= $data[rand(0,9)];
+            }
+          break;
+        case 'md5':
+          return md5(uniqid(mt_rand()));
+          break;
+        case 'hex':
+          if(($length%2) != 0 ){
+            $string = "Length must be even";
+          }
+          else {
+            $bytes = random_bytes($length/2);
+            $string = bin2hex($bytes);
+          }
+          break;
+        default:
+          $string = 'Your random type not found';
+          break;
+      }
         return $string;
     }
 }
