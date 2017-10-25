1354c1be53d92494edf14d7ac0747d75ff6a1760
Reza Juliandri <rezajuliandri20@gmail.com>
2017-10-25 10:28:29 +0700

    Add Development Class

---
+++ src/Development.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Development Class
+ */
+class Development
+{
+    static $start;
+    public static function timer()
+    {
+        if (is_null(self::$start))
+        {
+            self::$start = microtime(true);
+        }
+        else
+        {
+            $diff = round((microtime(true) - self::$start), 4);
+            self::$start = null;
+            return $diff;
+        }
+    }
+}
