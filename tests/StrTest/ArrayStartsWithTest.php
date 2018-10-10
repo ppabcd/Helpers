b8e2cd787f8d4b8ff25d853ac351d7328b61897b
Braunson Yager <Braunson@users.noreply.github.com>
2018-10-09 20:39:48 -0400

    Update ArrayStartsWithTest.php

--- tests/StrTest/ArrayStartsWithTest.php
+++ tests/StrTest/ArrayStartsWithTest.php
@@ -17,21 +17,6 @@ class ArrayStartsWithTest extends TestCase
         $processed = Str::array_keys_start_with($array, 'bread');
         $expected = ['bread' => 'wheat'];
         
-        $this->assertTrue($this->arrays_match($processed, $expected));
-    }
-    
-    protected function arrays_match($array1, $array2)
-    {
-        if (count(array_diff_assoc($array1, $array2))) {
-            return false;
-        }
-        
-        foreach($array1 as $k => $v) {
-            if ($v !== $array2[$k]) {
-                return false;
-            }
-        }
-
-        return true;
+        $this->assertTrue(Str::arrays_match($processed, $expected));
     }
 }
